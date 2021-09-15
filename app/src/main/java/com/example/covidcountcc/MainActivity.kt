b8f9a211301c55dedacdffa19c6c7fe8c0f6ad17
Ryumimo <ilhamsuherman80@gmail.com>
2021-09-15 22:52:41 +0700

    Menambahkan Splash Screen dan lain lain"

--- app/src/main/java/com/example/covidcountcc/MainActivity.kt
+++ app/src/main/java/com/example/covidcountcc/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.covidcountcc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ng.max.slideview.SlideView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
+        val slideView: SlideView = findViewById(R.id.slideview)
+        slideView.setOnSlideCompleteListener { // vibrate the device
+            // go to a new activity
+            startActivity(Intent(this@MainActivity , SplashScreen::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/covidcountcc/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.covidcountcc
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Vibrator
+import androidx.appcompat.app.AppCompatActivity
+import ng.max.slideview.SlideView
+
+class SplashScreen : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
